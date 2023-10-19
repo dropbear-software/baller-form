614b85bc079c242d158846e54e2946473e229074
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-10-19 13:47:48 +0000

    Add a temporary theme and some example documentation

--- src/BallerForm.ts
+++ src/BallerForm.ts
@@ -1,12 +1,22 @@
 import { html, css, LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 
+/**
+ * @tagname baller-form
+ * 
+ * @summary The Baller League Player Registration Form
+ * 
+ * @property {number} counter - some description
+ * @property {string} header - some description
+ *
+ * @cssproperty --baller-form-text-color - Controls the color of the text
+ */
 export class BallerForm extends LitElement {
   static styles = css`
     :host {
       display: block;
       padding: 25px;
-      color: var(--baller-form-text-color, #000);
+      color: var(--baller-form-text-color, var(--md-sys-color-on-surface));
     }
   `;
 
@@ -14,11 +24,11 @@ export class BallerForm extends LitElement {
 
   @property({ type: Number }) counter = 5;
 
-  __increment() {
+  private __increment() {
     this.counter += 1;
   }
 
-  render() {
+  protected render() {
     return html`
       <h2>${this.header} Nr. ${this.counter}!</h2>
       <button @click=${this.__increment}>increment</button>
