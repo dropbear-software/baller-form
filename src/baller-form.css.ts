45c189cbfc704a0b9a19704ac3776715f9f3e2d7
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-10-19 15:38:28 +0000

    Prettier fixes on CSS related files

--- src/baller-form.css.ts
+++ src/baller-form.css.ts
@@ -1,9 +1,9 @@
-import { css } from "lit";
+import { css } from 'lit';
 
 export const componentStyles = css`
-:host {
-  display: block;
-  padding: 25px;
-  color: var(--baller-form-text-color, var(--md-sys-color-on-surface));
-}
+  :host {
+    display: block;
+    padding: 25px;
+    color: var(--baller-form-text-color, var(--md-sys-color-on-surface));
+  }
 `;
