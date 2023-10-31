52f0e1b2cba463388fd62a3697c3280619aba001
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-10-31 13:27:33 +0000

    Minor refactoring of spam service

--- src/spam-service.ts
+++ src/spam-service.ts
@@ -9,11 +9,12 @@ export class SpamService {
 
   minimumTrustRequired = 0.5;
 
-  // Set to false to actually use the service otherwise simulate it passing for now
-  mockingEnabled = true;
+  // Simple flag to set if we are actually using this feature or not
+  featureEnabled = false;
 
   constructor(siteKey: string){
     this.siteKey = siteKey;
+    // https://developers.google.com/recaptcha/docs/v3#programmatically_invoke_the_challenge
     this.scriptURL.searchParams.append('render', siteKey);
   }
 
@@ -27,39 +28,38 @@ export class SpamService {
   // https://developers.google.com/recaptcha/docs/v3#programmatically_invoke_the_challenge
   isValidUser(){
     let calculatedScore = 0;
-    if('grecaptcha' in window) {
+    if (this.featureEnabled && 'grecaptcha' in window) {
       // @ts-ignore
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       window.grecaptcha.execute(this.siteKey, {action: 'submit'}).then(async (token: string) => {
-        if (this.mockingEnabled) {
-          calculatedScore = 1;
-          console.log(`The captcha token was: ${token}`);
-        } else {
-          // await this.validateTokenWithServer(token);
-          console.error("This service is not currently enabled");
-        }
+        await this.validateTokenWithServer(token);
       });
+    } else {
+      // Simulate a valid score here for now
+      calculatedScore = 1;
     }
 
     return (calculatedScore >= this.minimumTrustRequired);
   }
 
   // This function is responsible for sending the client token to the server side endpoint for validation
+  // eslint-disable-next-line class-methods-use-this, @typescript-eslint/no-unused-vars
   private async validateTokenWithServer(token: string) {
-    const response = await fetch(this.serverSideValidationEndpoint, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "same-origin", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({
-        "clientToken": token
-      }), // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+    throw new Error("Server side validation is not yet implemented");
+    
+    // const response = await fetch(this.serverSideValidationEndpoint, {
+    //   method: "POST", // *GET, POST, PUT, DELETE, etc.
+    //   mode: "cors", // no-cors, *cors, same-origin
+    //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //   credentials: "same-origin", // include, *same-origin, omit
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   redirect: "follow", // manual, *follow, error
+    //   referrerPolicy: "same-origin", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    //   body: JSON.stringify({
+    //     "clientToken": token
+    //   }), // body data type must match "Content-Type" header
+    // });
+    // return response.json(); // parses JSON response into native JavaScript objects
   }
 }
